1671b24c2a4486a8b6b9fd62ff3d2517afa503a2
lazar <lazarmatrak@mail.ru>
2022-05-11 19:34:06 +0200

    add event handlers for checkbox states

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+//Properties
+  
   password = '';
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+
+
+//Event Handlers
 
   onButtonClick(){
     this.password = 'MY PASSWORD';
   }
 
-  
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
 }
