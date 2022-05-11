6644ae680d78988130d80f6f84351f7f8e2f867a
lazar <lazarmatrak@mail.ru>
2022-05-11 19:24:05 +0200

    pw component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  password = '';
+
+  onButtonClick(){
+    this.password = 'MY PASSWORD';
+  }
+
+  
 }
